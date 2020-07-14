cbd5224bc34bb8d74fe80732bbae65a09cc03f36
Dario2018 <1191695933@qq.com>
2020-07-15 01:26:58 +0800

    第一次提交

---
+++ src/utils/validate.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Dario on 16/07/2020.
+ */
+
+/**
+ * @param {string} path
+ * @returns {Boolean}
+ */
+export function isExternal(path) {
+  return /^(https?:|mailto:|tel:)/.test(path)
+}
+
+/**
+ * @param {string} str
+ * @returns {Boolean}
+ */
+export function validUsername(str) {
+  const valid_map = ['admin', 'editor']
+  return valid_map.indexOf(str.trim()) >= 0
+}
+
+/**
+ * @param {string} str
+ * @returns {Boolean}
+ * @description 校验邮箱格式是否正确
+ *  */
+export function validEmail(str){
+  const match=/^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/
+  return match.test(str)
+} 
